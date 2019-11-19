3f2716dcf3f86984c025749ed200b08b17aacffe
ametz <ametz@DESKTOP-8J9N4BJ>
2019-11-19 13:44:20 +0100

    --primer commit

--- src/menuakGit/menuak.java
+++ src/menuakGit/menuak.java
@@ -1,10 +1,20 @@
 package menuakGit;
 
+import java.util.Scanner;
+
 public class menuak {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner scan=new Scanner(System.in);
+		int menu=0;
+		do{
+			System.out.println("Menu: 1-Suma, 2-Resta, 3-Multiplicacion, 4-Division");
+			menu=Integer.parseInt(scan.nextLine());
+			
+			
+		}
+		
 	}
 
 }
