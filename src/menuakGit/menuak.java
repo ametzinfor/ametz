a200623e3e2d5578daededee91c2b7cbf9846827
ametz <ametz@DESKTOP-8J9N4BJ>
2019-11-20 08:57:31 +0100

    --menu 4 opciones (zuzena)

--- src/menuakGit/menuak.java
+++ src/menuakGit/menuak.java
@@ -3,17 +3,43 @@ package menuakGit;
 import java.util.Scanner;
 
 public class menuak {
-
+	
+	
 	public static void main(String[] args) {
 		
 		Scanner scan=new Scanner(System.in);
-		int menu=0;
+		int aukera=0;
+		int z1=0;
+		int z2=0; 
 		do{
 			System.out.println("Menu: 1-Suma, 2-Resta, 3-Multiplicacion, 4-Division");
-			menu=Integer.parseInt(scan.nextLine());
+			aukera=Integer.parseInt(scan.nextLine());
+		
+			System.out.println("Sartu lehenengo zenbakia");
+			z1=Integer.parseInt(scan.nextLine());
+			System.out.println("Sartu bigarren zenbakia");
+			z2=Integer.parseInt(scan.nextLine());
+			
+			switch (aukera){
+			
+			case 1: // 1 aukera Suma
+				System.out.println("Suma");
+				break;
+			case 2: // 2 aukera resta
+				System.out.println("Resta");
+				break;
+				
+			case 3: // 3 aukera multiplicación
+				System.out.println("Multiplicacion");
+				break;
+			case 4: // 4 aukera dividir
+				System.out.println("Dividir");
+				break;
 			
 			
-		}while(menu!=0);
+			}
+			
+		}while(aukera!=0);
 		
 	}
 
